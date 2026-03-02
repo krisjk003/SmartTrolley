afec9f3e8a20e3cf1cc85dc14bad68c5b5227358
Jyothish kumar <vishnujayakumar003@gmail.com>
2026-03-02 21:37:48 +0530

    Fixed PORT for Render

--- back end/server.js
+++ back end/server.js
@@ -55,6 +55,8 @@ app.post("/clear", (req, res) => {
     res.json({ success: true });
 });
 
-app.listen(5000, "0.0.0.0", () => {
-    console.log("Server running at http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
